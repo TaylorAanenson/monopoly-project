1ae8b28f7cd17fbbbc02d6dc55b64a2f5256c782
Tristin Tobias <tristintobias@gmail.com>
2018-07-15 17:48:01 -0700

    added objects for each property; some basic styling changes to line height in info divs, and fixed JS messaging for submit player info.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -32,6 +32,130 @@ var boardPosition = ['start', 'goldenGatePark', 'huntersPoint', 'sunset', 'nobHi
                     'missionDistrict','chinaTown','haightAshbury','chance','theCastro','civicCenter','AlamoSquare',
                     'dogPatch','fog','northBeach','soma','financialDistrict','russianHill'];
 
+// objects for each property
+
+
+var start = {
+    name: "GO!",
+    price: 200,
+    url: 'assets/images/prop-imgs/go.jpg'
+};
+var goldenGatePark = {
+    name: "Golden Gate Park",
+    price: 100,
+    rent: 50,
+    url: 'assets/images/prop-imgs/goldenGatePark.jpg'
+};
+var huntersPoint = {
+    name: "Hunter's Point",
+    price: 120,
+    rent: 60,
+    url: 'assets/images/prop-imgs/huntersPoint.jpg'
+};
+var sunset = {
+    name: "Sunset",
+    price: 140,
+    rent: 70,
+    url: 'assets/images/prop-imgs/sunset.jpg'
+};
+var nobHill = {
+    name: "Nob Hill",
+    price: 160,
+    rent: 80,
+    url: 'assets/images/prop-imgs/nobHill.jpg'
+};
+var oneWayStreet = {
+    name: "Wrong way! One Way Street. Lose one turn.",
+    price: 0,
+    rent: 0,
+    url: 'assets/images/prop-imgs/oneWayStreet.jpg'
+};
+var noeValley = {
+    name: "Noe Valley",
+    price: 180,
+    rent: 90,
+    url: 'assets/images/prop-imgs/noeValley.jpg'
+};
+var missionDistrict = {
+    name: "Mission District",
+    price: 200,
+    rent: 100,
+    url: 'assets/images/prop-imgs/missionDistrict.jpg'
+};
+
+var chinaTown = {
+    name: "China Town",
+    price: 220,
+    rent: 110,
+    url: 'assets/images/prop-imgs/chinaTown.jpg'
+};
+var haightAshbury = {
+    name: "Haight Ashbury",
+    price: 240,
+    rent: 120,
+    url: 'assets/images/prop-imgs/haightAshbury.jpg'
+};
+var chance = {
+    name: "Chance! Take a chance card and follow the instructions.",
+    price: 0,
+    rent: 0,
+    url: 'assets/images/prop-imgs/chance.jpg'
+};
+var theCastro = {
+    name: "The Castro",
+    price: 260,
+    rent: 130,
+    url: 'assets/images/prop-imgs/theCastro.jpg'
+};
+var civicCenter = {
+    name: "Civic Center",
+    price: 280,
+    rent: 140,
+    url: 'assets/images/prop-imgs/civicCenter.jpg'
+};
+var alamoSquare = {
+    name: "Alamo Square",
+    price: 300,
+    rent: 150,
+    url: 'assets/images/prop-imgs/alamoSquare.jpg'
+};
+var dogPatch = {
+    name: "Dog Patch",
+    price: 300,
+    rent: 150,
+    url: 'assets/images/prop-imgs/dogPatch.jpg'
+};
+var fog = {
+    name: "Lost in the fog. Lose one turn.",
+    price: 0,
+    rent: 0,
+    url: 'assets/images/prop-imgs/fog.jpg'
+};
+var northBeach = {
+    name: "North Beach",
+    price: 320,
+    rent: 160,
+    url: 'assets/images/prop-imgs/northBeach.jpg'
+};
+var soma = {
+    name: "SOMA",
+    price: 340,
+    rent: 170,
+    url: 'assets/images/prop-imgs/northBeach.jpg'
+};
+var financialDistrict = {
+    name: "Financial District",
+    price: 360,
+    rent: 180,
+    url: 'assets/images/prop-imgs/financialDistrict.jpg'
+};
+var russianHill = {
+    name: "Russian Hill",
+    price: 380,
+    rent: 190,
+    url: 'assets/images/prop-imgs/russianHill.jpg'
+};
+
 //////////////////////////////////////////////////FUNCTIONS//////////////////////////////////////////////////
 
 function updateTurn(player){
@@ -124,11 +248,11 @@ if (ranDieTwo == ranDieOne){
 $(document).on('click', '#submitPlayerInfo', function (){
     event.preventDefault();
     if ($('#playerOneName').val() == '' || $('#playerTwoName').val() == ''){
-        alert('Please enter your name, Player One.');
+        alert('Please enter a name for both players.');
     }else if (playerOne.token == undefined || playerTwo.token == undefined){
-        alert('Please choose a token, Player One.')
+        alert('Please choose a token for both players')
     }else if (playerOne.firstroll == undefined || playerTwo.firstroll == undefined){
-        alert('Please click the die to roll, Player One.')
+        alert('Please click the die to roll for each player.')
     }else {
         playerOne.name = $('#playerOneName').val();
         playerTwo.name = $('#playerTwoName').val();
